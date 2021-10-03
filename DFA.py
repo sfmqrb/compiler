ba4958c22c3d1b1315645f479aa165956caf6ffd
Sajad Faghfoor Maghrebi <sajad.faghfoor@gmail.com>
2021-10-03 09:30:19 +0330

    prettified

--- DFA.py
+++ DFA.py
@@ -4,7 +4,7 @@ import re
 class STATE(Enum):
     START = 0
     ENDBACK = 200
-    END = 0
+    END = 100
     NUM = 1
     IDandKEYWORDS = 3
     EQU = 5
@@ -12,9 +12,9 @@ class STATE(Enum):
     COMBGN = 8
     ONELINECOM = 9
     TWOLINECOMBGN = 11
-    TWOLINECOMEND = 12 
+    TWOLINECOMEND = 12
     ERROR = -1
-        
+
 
 MY_DIGIT = re.compile(r'[0-9]')
 MY_LETTER = re.compile(r'[a-zA-Z]')
@@ -125,78 +125,80 @@ class STATES_TRANS():
             next_state = STATE.TWOLINECOMBGN
             return next_state, 'valid'
 
-def get_next_state(this_state, this_char):
+    def next_state_after_ERROR(this_char):
+        next_state = STATE.START
+        return next_state, 'ERROR'
+
+    def next_state_after_ENDBACK(this_char):
+        next_state = STATE.END
+        return next_state, 'valid'
+
+    def next_state_after_END(this_char):
+        next_state = STATE.START
+        return next_state, 'valid'
+
+
+def get_next_state(this_state, this_char, idx):
     if this_state == STATE.START:
         next_state, log = STATES_TRANS.next_state_after_START(this_char)
-        return next_state, log
-        
+        return next_state, log, idx+1
+
     if this_state == STATE.NUM:
         next_state, log = STATES_TRANS.next_state_after_NUM(this_char)
-        return next_state, log
+        return next_state, log, idx+1
 
     if this_state == STATE.IDandKEYWORDS:
         next_state, log = STATES_TRANS.next_state_after_IDandKEYWORDS(this_char)
-        return next_state, log
-    
+        return next_state, log, idx + 1
+
     if this_state == STATE.EQU:
         next_state, log = STATES_TRANS.next_state_after_EQU(this_char)
-        return next_state, log
+        return next_state, log, idx + 1
 
     if this_state == STATE.WHITESPACE:
         next_state, log = STATES_TRANS.next_state_after_WHITESPACE(this_char)
-        return next_state, log
+        return next_state, log, idx + 1
 
     if this_state == STATE.COMBGN:
         next_state, log = STATES_TRANS.next_state_after_COMBGN(this_char)
-        return next_state, log
+        return next_state, log, idx + 1
 
     if this_state == STATE.ONELINECOM:
         next_state, log = STATES_TRANS.next_state_after_ONELINECOM(this_char)
-        return next_state, log
+        return next_state, log, idx + 1
 
     if this_state == STATE.TWOLINECOMBGN:
         next_state, log = STATES_TRANS.next_state_after_TWOLINECOMBGN(this_char)
-        return next_state, log
+        return next_state, log, idx + 1
 
 
     if this_state == STATE.TWOLINECOMEND:
         next_state, log = STATES_TRANS.next_state_after_TWOLINECOMEND(this_char)
-        return next_state, log
+        return next_state, log, idx + 1
 
     if this_state == STATE.ERROR:
-        next_state, log = STATE.START, 'ERROR'
-        return next_state, log
+        next_state, log = STATES_TRANS.next_state_after_ERROR(this_char)
+        return next_state, log, idx + 1
 
     if this_state == STATE.END:
-        next_state, log = STATE.START, 'end'
-        return next_state, log
-    
+        next_state, log = STATES_TRANS.next_state_after_END(this_char)
+        return next_state, log, idx
+
     if this_state == STATE.ENDBACK:
-        next_state, log = STATE.START, 'endback'
-        return next_state, log
-    
+        next_state, log = STATES_TRANS.next_state_after_ENDBACK(this_char)
+        return next_state, log, idx
+
 
 if __name__ == "__main__":
     # s = "void main ( void ) {\n    int a = 0;\n    // comment1\n    a = 2 + 2;\n    a = a - 3;\n    cde = a;\n    if (b /* comment2 */ == 3d) {\n        a = 3;\n        cd!e = 7;\n    }\n    else */\n    {\n        b = a < cde;\n        {cde = @2;\n    }}\n    return;/* comment 3}"
     s = "/* comment 3}*/ sajad == 10 "
     this_char_idx = 0
-    next_state = STATE.START
-    
+    this_state = STATE.START
+
+    print("{: >10} {: >20} {: >15} {: >20}".format(*['this_char', 'this_state', 'this_char_idx', 'next_state']))
     while this_char_idx < len(s):
-        # print("char_idx -- nextState -- log", this_char_idx, next_state)
-        # print()
-        this_state = next_state
         this_char = s[this_char_idx]
-        next_state, log = get_next_state(this_state, this_char)
-        
-        print("char -- thisState -- log -- idx -- nextState", this_char, this_state, log, this_char_idx, next_state)
-
-
-
-        # if next_state == STATE.END or next_state == STATE.ENDBACK or next_state == STATE.ERROR:
-        #     print("token found")
-
-        if next_state == STATE.ENDBACK:
-            next_state = STATE.START
-            this_char_idx -= 1    
-        this_char_idx += 1
+        next_state, log, next_char_idx = get_next_state(this_state, this_char, this_char_idx)
+        print("{: >10} {: >20} {: >15} {: >20}".format(*[this_char, this_state, this_char_idx, next_state]))
+        this_char_idx = next_char_idx
+        this_state = next_state
