5c7b551939a7b1a482c12359d1bb293f983a2eb5
Sajad Faghfoor Maghrebi <sajad.faghfoor@gmail.com>
2021-10-03 16:39:52 +0330

    first2tests checked

--- DFA.py
+++ DFA.py
@@ -1,7 +1,6 @@
 from enum import Enum
 import re
-from sre_constants import IN_IGNORE
-# from typing_extensions import Required
+
 
 class STATE(Enum):
     START = 0
@@ -23,12 +22,15 @@ MY_DIGIT = re.compile(r'[0-9]')
 MY_LETTER = re.compile(r'[a-zA-Z]')
 MY_LETDIG = re.compile(r'[a-zA-Z0-9]')
 MY_SYMB = re.compile(r'[\[\]\(\)\{\}\;\:\-\+\<\,]')
-KEYWORDS = re.compile(r'^if$|^else$|^void$|^int$|^repeat$|^break$|^until$|^return$')
+KEYWORDS = re.compile(
+    r'^if$|^else$|^void$|^int$|^repeat$|^break$|^until$|^return$')
 MY_WHITESPACE = re.compile(' |\t|\n|\r|\v|\f')
 MY_FORSLASH = re.compile('\/')
 MY_STAR = re.compile('\*')
 MY_EQ = re.compile('\=')
 MY_NEWLINE = re.compile('\n')
+MY_ALPHABET = re.compile(
+    r'[a-zA-Z0-9]|[\[\]\(\)\{\}\;\:\-\+\<\,\=\/\*]|[\s|\t|\n|\r|\v|\f]')
 
 
 def get_token_type(raw_token_type, token):
@@ -65,7 +67,7 @@ class STATES_TRANS():
             next_state = STATE.WHITESPACE
             return next_state, ''
         else:
-            next_state = STATE.ERROR # error
+            next_state = STATE.ERROR  # error
             return next_state, 'Invalid input'
 
     def next_state_after_NUM(this_char):
@@ -75,25 +77,34 @@ class STATES_TRANS():
         elif re.match(MY_LETTER, this_char) != None:
             next_state = STATE.ERROR
             return next_state, 'Invalid Number'
-        else:
+        elif re.match(MY_ALPHABET, this_char) != None:
             next_state = STATE.ENDBACK
             return next_state, 'NUM'
+        else:
+            next_state = STATE.ERROR  # error
+            return next_state, 'Invalid input'
 
     def next_state_after_IDandKEYWORDS(this_char):
         if re.match(MY_LETDIG, this_char) != None:
             next_state = STATE.IDandKEYWORDS
             return next_state, ''
-        else:
+        elif re.match(MY_ALPHABET, this_char) != None:
             next_state = STATE.ENDBACK
             return next_state, 'IDorKeywords'
+        else:
+            next_state = STATE.ERROR  # error
+            return next_state, 'Invalid input'
 
     def next_state_after_EQU(this_char):
         if re.match(MY_EQ, this_char) != None:
             next_state = STATE.END
             return next_state, 'SYMBOL'
-        else:
+        elif re.match(MY_ALPHABET, this_char) != None:
             next_state = STATE.ENDBACK
             return next_state, 'SYMBOL'
+        else:
+            next_state = STATE.ERROR
+            return next_state, 'Invalid input'
 
     def next_state_after_WHITESPACE(this_char):
         # if re.match(MY_WHITESPACE, this_char) != None:
@@ -102,7 +113,7 @@ class STATES_TRANS():
         # else:
         #     next_state = STATE.ENDBACK
         #     return next_state, 'WHITESPACE'
-        next_state = STATE.ENDBACK   # Why did not STATE.END work?
+        next_state = STATE.ENDBACK
         return next_state, 'WHITESPACE'
 
     def next_state_after_COMBGN(this_char):
@@ -143,13 +154,16 @@ class STATES_TRANS():
             next_state = STATE.TWOLINECOMBGN
             return next_state, ''
 
-    def next_state_after_STAR(this_char):
+    def next_state_after_STAR(this_char):  # just for begining star
         if re.match(MY_FORSLASH, this_char) != None:
             next_state = STATE.ERROR
             return next_state, 'Unmatched comment'
-        else:
-            next_state = STATE.END
+        elif re.match(MY_ALPHABET, this_char) != None:
+            next_state = STATE.ENDBACK
             return next_state, 'SYMBOL'
+        else:
+            next_state = STATE.ERROR
+            return next_state, 'Invalid input'
 
     def next_state_after_ERROR(this_char):
         next_state = STATE.START
@@ -167,14 +181,15 @@ class STATES_TRANS():
 def get_next_state(this_state, this_char, idx):
     if this_state == STATE.START:
         next_state, log = STATES_TRANS.next_state_after_START(this_char)
-        return next_state, log, idx+1
+        return next_state, log, idx + 1
 
     if this_state == STATE.NUM:
         next_state, log = STATES_TRANS.next_state_after_NUM(this_char)
-        return next_state, log, idx+1
+        return next_state, log, idx + 1
 
     if this_state == STATE.IDandKEYWORDS:
-        next_state, log = STATES_TRANS.next_state_after_IDandKEYWORDS(this_char)
+        next_state, log = STATES_TRANS.next_state_after_IDandKEYWORDS(
+            this_char)
         return next_state, log, idx + 1
 
     if this_state == STATE.EQU:
@@ -194,11 +209,13 @@ def get_next_state(this_state, this_char, idx):
         return next_state, log, idx + 1
 
     if this_state == STATE.TWOLINECOMBGN:
-        next_state, log = STATES_TRANS.next_state_after_TWOLINECOMBGN(this_char)
+        next_state, log = STATES_TRANS.next_state_after_TWOLINECOMBGN(
+            this_char)
         return next_state, log, idx + 1
 
     if this_state == STATE.TWOLINECOMEND:
-        next_state, log = STATES_TRANS.next_state_after_TWOLINECOMEND(this_char)
+        next_state, log = STATES_TRANS.next_state_after_TWOLINECOMEND(
+            this_char)
         return next_state, log, idx + 1
 
     if this_state == STATE.STAR:
@@ -221,30 +238,40 @@ def get_next_state(this_state, this_char, idx):
 # def get_next_token():
 #     next_state, log, next_char_idx = get_next_state(this_state, this_char, this_char_idx)
 
+
 def get_final_token(this_state, s, addition_str, start_token, line, token):
     if this_state == STATE.TWOLINECOMBGN or this_state == STATE.TWOLINECOMEND:
         end_token = len(s) - len(addition_str)
         token = s[start_token:end_token]
         return line, token, 'Unclosed comment', start_token, end_token
-    else: 
-        return None, None, None, None, None  
+    else:
+        return None, None, None, None, None
+
 
 def print_letter_state(this_char_idx, this_state, this_char, next_state, log):
     if re.match(MY_NEWLINE, this_char):
         print("{: >10} {: >20} {: >15} {: >20} {: >10}".format(
-                *['NEWLINE', this_state, this_char_idx, next_state, log]))
+            *['NEWLINE', this_state, this_char_idx, next_state, log]))
     else:
         print("{: >10} {: >20} {: >15} {: >20} {: >10}".format(
-                *[this_char, this_state, this_char_idx, next_state, log]))
+            *[this_char, this_state, this_char_idx, next_state, log]))
+
 
 if __name__ == "__main__":
     # s = "void main ( void ) {\n \n\n   int a = 0;\n    // comment1\n    a = 2 + 2;\n    a = a - 3;\n    cde = a;\n    if (b /* comment2 */ == 3d) {\n        a = 3;\n        cd!e = 7;\n    }\n    else */\n    {\n        b = a < cde;\n        {cde = @2;\n    }}\n    return;/* comment 3}"
     # s = "void main(void) {"
     # s = 'ifif  dfs '
-    # s = "void main*/\nreturn;/* comment 3}"
-    s = "d3d = 10}; 100\nx=10 // a = 10  \n /* dsfds */"
+    # s = "void main*/\nreturn;/* comment 3}*/\ns=10; s=s+1;//#$@#$#@%^$#%$"
+    # s = "@sajad@ hello  -1 == 10;"
+    # s = "d3d = 10}; 100\nx=10 // a = 10  \n /* dsfds */"
 
-    addition_str = "  \n  "
+    f = open('./pa_1/PA1_testcases1.2/T02/input.txt', 'r')
+    ls_text = f.readlines()
+    s = ""
+    for txt in ls_text:
+        s += txt
+
+    addition_str = "\n  "  # has to begin with \n and be longer than 1
     s += addition_str
     this_char_idx = 0
     this_state = STATE.START
@@ -256,15 +283,16 @@ if __name__ == "__main__":
         if this_state == STATE.START:
             start_token = this_char_idx
         this_char = s[this_char_idx]
-        next_state, log, next_char_idx = get_next_state(this_state, this_char, this_char_idx)
+        next_state, log, next_char_idx = get_next_state(
+            this_state, this_char, this_char_idx)
 
         # print_letter_state(this_char_idx, this_state, this_char, next_state, log)
 
-        if (next_state == STATE.ENDBACK  or next_state == STATE.END or next_state == STATE.ERROR) and log:
+        if (next_state == STATE.ENDBACK or next_state == STATE.END
+                or next_state == STATE.ERROR) and log:
             raw_token_type = log
 
-
-        if this_state == STATE.END:
+        if this_state == STATE.END or this_state == STATE.ERROR:
             end_token = this_char_idx
             token = s[start_token:end_token]
             token_type = get_token_type(raw_token_type, token)
@@ -283,7 +311,8 @@ if __name__ == "__main__":
         this_char_idx = next_char_idx
         this_state = next_state
 
-    line, token, log, start_token, end_token = get_final_token(this_state, s, addition_str, start_token, line, token)
-    if line:
+    line, token, log, start_token, end_token = get_final_token(
+        this_state, s, addition_str, start_token, line, token)
+    if line != None:
         print("line {: >5} {: >20} {: >20} {: >10} {: >10}".format(
             line, token, log, start_token, end_token))
