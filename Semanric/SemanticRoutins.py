4b9af0740cc642a1bfa7900339c685189d7303b0
amm266 <a.m.mohammadi266@gmail.com>
2022-01-30 16:40:44 +0330

    add semantic_stack and inter_code

--- Semanric/SemanticRoutins.py
+++ Semanric/SemanticRoutins.py
@@ -1,2 +1,11 @@
-def calc(find_adr, get_temp):
+semantic_stack = []
+inter_code = []
+
+
+def func_calc(find_adr, get_temp):
+    find_adr('')
+    pass
+
+
+def func_add(find_adr, get_temp):
     pass
