961be17a6a89ea40270510321e789c3baae4a46a
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-01-31 19:20:23 +0330

    before checkout

--- Semanric/SemanticRoutins.py
+++ Semanric/SemanticRoutins.py
@@ -2,18 +2,9 @@ semantic_stack = []
 program_block = []
 
 
+####################### Test Routines #########################
 def test():
     print("xxx")
-
-
-def func_calc(find_adr, get_temp, input_token):
-    find_adr('')
-    get_temp()
-    pass
-
-
-def func_add(find_adr, get_temp, input_token):
-    pass
 
 
 def func_test_pid(find_adr, get_temp, input_token):
@@ -22,16 +13,40 @@ def func_test_pid(find_adr, get_temp, input_token):
 
 
 def func_test_assign(find_adr, get_temp, input_token):
-    program_block.append("(ASSIGN," + str(semantic_stack.pop()) + "," + str(semantic_stack.pop()) + ",)")
+    program_block.append(
+        "(ASSIGN," + str(semantic_stack.pop()) + "," + str(semantic_stack.pop()) + ",)"
+    )
 
 
 def func_test_add(find_adr, get_temp, input_token):
     t = get_temp()
-    program_block.append("(ADD," + str(semantic_stack.pop()) + "," + str(semantic_stack.pop()) + "," + str(t) + ")")
+    program_block.append(
+        "(ADD,"
+        + str(semantic_stack.pop())
+        + ","
+        + str(semantic_stack.pop())
+        + ","
+        + str(t)
+        + ")"
+    )
     semantic_stack.append(t)
 
 
 def func_test_mult(find_adr, get_temp, input_token):
     t = get_temp()
-    program_block.append("(MULT," + str(semantic_stack.pop()) + "," + str(semantic_stack.pop()) + "," + str(t) + ")")
+    program_block.append(
+        "(MULT,"
+        + str(semantic_stack.pop())
+        + ","
+        + str(semantic_stack.pop())
+        + ","
+        + str(t)
+        + ")"
+    )
     semantic_stack.append(t)
+
+
+####################### Main Routines #########################
+def func_pid(find_adr, get_temp, input_token):
+    p = find_adr(input_token)
+    semantic_stack.append(p)
