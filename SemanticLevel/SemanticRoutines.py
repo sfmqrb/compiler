adfe31dde4d0b47843de682ff6b443611b506927
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-01-31 23:24:38 +0330

    add and sub handled

--- SemanticLevel/SemanticRoutines.py
+++ SemanticLevel/SemanticRoutines.py
@@ -4,6 +4,8 @@ program_block = []
 semantic_instance = None
 
 ####################### Test Routines #########################
+
+
 def test():
     print("xxx")
 
@@ -15,7 +17,8 @@ def func_test_pid(find_adr, get_temp, input_token):
 
 def func_test_assign(find_adr, get_temp, input_token):
     program_block.append(
-        "(ASSIGN," + str(semantic_stack.pop()) + "," + str(semantic_stack.pop()) + ",)"
+        "(ASSIGN," + str(semantic_stack.pop()) +
+        "," + str(semantic_stack.pop()) + ",)"
     )
 
 
@@ -49,23 +52,32 @@ def func_test_mult(find_adr, get_temp, input_token):
 
 ####################### Main Routines #########################
 def func_pid(find_adr, get_temp, input_token):
-    # p = find_adr(input_token)
-    # semantic_stack.append(p)
+    p = find_adr(input_token)
+    semantic_stack.append(p)
     pass
 
 
 def func_pnum(find_adr, get_temp, input_token):
-    # semantic_stack.append("#" + input_token)
+    semantic_stack.append("#" + input_token)
     pass
 
 
 def func_assign(find_adr, get_temp, input_token):
-    # _from = semantic_stack.pop()
-    # _to = semantic_stack.pop()
-    # program_block.append("(ASSIGN," + str(_from) + "," + str(_to) + ",)")
+    _from = semantic_stack.pop()
+    _to = semantic_stack.pop()
+    program_block.append("(ASSIGN," + str(_from) + "," + str(_to) + ",)")
     pass
 
 
 def func_push(find_adr, get_temp, input_token):
-    # semantic_stack.append(input_token)
+    semantic_stack.append(input_token)
     pass
+
+
+def func_add_op(find_adr, get_temp, input_token):
+    first = semantic_stack.pop()
+    action = "ADD" if semantic_stack.pop() == "+" else "SUB"
+    second = semantic_stack.pop()
+    t = get_temp()
+    program_block.append(f"({action}, {str(first)}, {str(second)}, {str(t)})")
+    semantic_stack.append(t)
