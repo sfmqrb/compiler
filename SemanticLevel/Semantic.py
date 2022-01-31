47dfe1652d5f26b295da60889f5ca5687be721b3
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-01-31 21:25:18 +0330

    delete vscode

--- SemanticLevel/Semantic.py
+++ SemanticLevel/Semantic.py
@@ -29,8 +29,7 @@ class TempManager:
 
 
 class Semantic:
-    name_function_dict = {
-    }
+    name_function_dict = {}
     parse_table = None
     temp_manager = None
     errors = []
@@ -41,25 +40,65 @@ class Semantic:
         instance = self
 
     def run(self, func_name, input_token):
-        func_name = func_name[1:len(func_name)]
+        func_name = func_name[1 : len(func_name)]
         try:
-            getattr(SemanticRoutines, "func_" + func_name)(self.parse_table.get_adr, self.temp_manager.get_temp,
-                                                           input_token)
+            getattr(SemanticRoutines, "func_" + func_name)(
+                self.parse_table.get_adr, self.temp_manager.get_temp, input_token
+            )
         except:
+            print("SemanticLevel Error! Function '" + func_name + "' not found")
             pass
 
     def error(self, err_type, line_number, id, expected, illegal, arg):
         line_number = str(line_number)
         if err_type == ErrorType.scoping:
-            err = "#" + line_number + ": SemanticLevel Error! '" + id + "' is not defined"
+            err = (
+                "#" + line_number + ": SemanticLevel Error! '" + id + "' is not defined"
+            )
         elif err_type == ErrorType.void_type:
-            err = "#" + line_number + ": SemanticLevel Error! Illegal type of void for '" + id + "'"
+            err = (
+                "#"
+                + line_number
+                + ": SemanticLevel Error! Illegal type of void for '"
+                + id
+                + "'"
+            )
         elif err_type == ErrorType.number_mathing:
-            err = "#" + line_number + ":semantic error! Mismatch in numbers of arguments of '" + id + "'"
+            err = (
+                "#"
+                + line_number
+                + ":semantic error! Mismatch in numbers of arguments of '"
+                + id
+                + "'"
+            )
         elif err_type == ErrorType.break_stmt:
-            err = "#" + line_number + ": SemanticLevel Error! No 'while' or 'switch' found for 'break'"
+            err = (
+                "#"
+                + line_number
+                + ": SemanticLevel Error! No 'while' or 'switch' found for 'break'"
+            )
         elif err_type == ErrorType.type_mismatch:
-            err = "#" + line_number + ": SemanticLevel Error! Type mismatch in operands, Got '" + illegal + "' instead of '" + expected + "'"
+            err = (
+                "#"
+                + line_number
+                + ": SemanticLevel Error! Type mismatch in operands, Got '"
+                + illegal
+                + "' instead of '"
+                + expected
+                + "'"
+            )
         elif err_type == ErrorType.type_matching:
-            err = "#" + line_number + ": SemanticLevel Error!Mismatch in type of argument " + arg + " for '" + id + "'. Expected '" + expected + "' but got '" + illegal + "' instead "
+            err = (
+                "#"
+                + line_number
+                + ": SemanticLevel Error!Mismatch in type of argument "
+                + arg
+                + " for '"
+                + id
+                + "'. Expected '"
+                + expected
+                + "' but got '"
+                + illegal
+                + "' instead "
+            )
         self.errors.append(err)
