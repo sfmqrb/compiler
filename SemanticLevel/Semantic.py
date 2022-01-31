0ee9528ba0216bb5cfbfe353297125c2f82073b3
amm266 <a.m.mohammadi266@gmail.com>
2022-01-31 20:46:44 +0330

    before pull

--- SemanticLevel/Semantic.py
+++ SemanticLevel/Semantic.py
@@ -1,11 +1,12 @@
-from debugpy.common.json import enum
+import enum
 
-import SemanticRoutines
+from SemanticLevel import SemanticRoutines
 
 instance = None
+SemanticRoutines.semantic_instance = instance
 
 
-class ErrorType(enum):
+class ErrorType(enum.Enum):
     scoping = 1
     void_type = 2
     number_mathing = 3
@@ -50,15 +51,15 @@ class Semantic:
     def error(self, err_type, line_number, id, expected, illegal, arg):
         line_number = str(line_number)
         if err_type == ErrorType.scoping:
-            err = "#" + line_number + ": Semantic Error! '" + id + "' is not defined"
+            err = "#" + line_number + ": SemanticLevel Error! '" + id + "' is not defined"
         elif err_type == ErrorType.void_type:
-            err = "#" + line_number + ": Semantic Error! Illegal type of void for '" + id + "'"
+            err = "#" + line_number + ": SemanticLevel Error! Illegal type of void for '" + id + "'"
         elif err_type == ErrorType.number_mathing:
             err = "#" + line_number + ":semantic error! Mismatch in numbers of arguments of '" + id + "'"
         elif err_type == ErrorType.break_stmt:
-            err = "#" + line_number + ": Semantic Error! No 'while' or 'switch' found for 'break'"
+            err = "#" + line_number + ": SemanticLevel Error! No 'while' or 'switch' found for 'break'"
         elif err_type == ErrorType.type_mismatch:
-            err = "#" + line_number + ": Semantic Error! Type mismatch in operands, Got '" + illegal + "' instead of '" + expected + "'"
+            err = "#" + line_number + ": SemanticLevel Error! Type mismatch in operands, Got '" + illegal + "' instead of '" + expected + "'"
         elif err_type == ErrorType.type_matching:
-            err = "#" + line_number + ": Semantic Error!Mismatch in type of argument " + arg + " for '" + id + "'. Expected '" + expected + "' but got '" + illegal + "' instead "
+            err = "#" + line_number + ": SemanticLevel Error!Mismatch in type of argument " + arg + " for '" + id + "'. Expected '" + expected + "' but got '" + illegal + "' instead "
         self.errors.append(err)
