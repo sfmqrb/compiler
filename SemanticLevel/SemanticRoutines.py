d00142847e8a712f893eb54590d9d4685a0d5fdb
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-02-01 13:37:01 +0330

    delete test routines

--- SemanticLevel/SemanticRoutines.py
+++ SemanticLevel/SemanticRoutines.py
@@ -15,53 +15,6 @@ ARG_COUNT = 0
 def get_PB_next():
     return len(program_block)
 
-####################### Test Routines #########################
-
-
-def test():
-    print("xxx")
-
-
-def func_test_pid(find_adr, get_temp, input_token):
-    p = find_adr(input_token)
-    semantic_stack.append(p)
-
-
-def func_test_assign(find_adr, get_temp, input_token):
-    program_block.append(
-        "(ASSIGN," + str(semantic_stack.pop()) +
-        "," + str(semantic_stack.pop()) + ",)"
-    )
-
-
-def func_test_add(find_adr, get_temp, input_token):
-    t = get_temp()
-    program_block.append(
-        "(ADD,"
-        + str(semantic_stack.pop())
-        + ","
-        + str(semantic_stack.pop())
-        + ","
-        + str(t)
-        + ")"
-    )
-    semantic_stack.append(t)
-
-
-def func_test_mult(find_adr, get_temp, input_token):
-    t = get_temp()
-    program_block.append(
-        "(MULT,"
-        + str(semantic_stack.pop())
-        + ","
-        + str(semantic_stack.pop())
-        + ","
-        + str(t)
-        + ")"
-    )
-    semantic_stack.append(t)
-
-
 ####################### Main Routines #########################
 
 
